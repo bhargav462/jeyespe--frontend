a5dec89b07828deec0878d108218a2235f7c6507
Bhavuk-Garg <38847938+Bhavuk-Garg@users.noreply.github.com>
2020-11-02 16:01:12 +0530

    added zoom image

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,9 +1,43 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import ReactImageMagnify from 'react-image-magnify';
+import Img1 from './placeholder.jpg'
+import Img2 from './1.jpg'
 export default function ProductDetails({match}) {
+    let arr=[Img1,Img2,Img2]
+    const [shownImage,changeImage]=useState(0)
     const productId=match.params.id;
-    return <div>
-           
+    return <div style={{display:'flex',margin:'30px'}}>
+        <div style={{display:'flex',flexDirection:'column',marginRight:'10px'}}>
+            {
+                arr.map((img,idx)=>{
+                    return <div style={{width:'60px',height:'70px',backgroundImage:`url(${img})`,
+                        backgroundSize:'contain',marginBottom:'5px'}}
+                            onClick={()=> changeImage(idx)}>
+                        </div>
+                })
+            }
+
+            
+             
+        </div>
+        <div style={{width:'400px',height:'400px',backgroundColor:'red'}}>
+           <ReactImageMagnify {...{
+            smallImage: {
+                alt: 'Wristwatch by Ted Baker London',
+                isFluidWidth: true,
+                src: arr[shownImage],
+            },
+            largeImage: {
+                src: arr[shownImage],
+                width: 1000,
+                height: 1000
+            },
+            shouldUsePositiveSpaceLens: true,
+            isHintEnabled:true,
+        }} />
+        </div>
+        <h1>Hello World</h1>
+        <button onClick={()=>changeImage((shownImage+arr.length-1)%arr.length)}>Left</button>
         </div>
     
 }
