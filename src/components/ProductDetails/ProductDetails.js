f7ca35e8c0f4ea7b4c43432133c96a49471626e8
Nakkina Bhargav <43701429+bhargav462@users.noreply.github.com>
2020-11-15 19:35:54 +0530

    buyNow option

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -5,10 +5,55 @@ import Img2 from './1.jpg'
 import Img3 from './2.jpg'
 import {StyledButton} from '../utility/StyledButton'
 import Button from '@material-ui/core/Button';
+import StripeCheckout from 'react-stripe-checkout';
+import Cookies from 'js-cookie'
+
 export default function ProductDetails({match}) {
     let arr=[Img1,Img2,Img3]
     const [shownImage,changeImage]=useState(0)
     const productId=match.params.id;
+
+    function authentication(response) {
+        if (!response.ok) {
+          if (response.status === 403) {
+            response.json().then((element) => {
+              if (element.error === "Login") {
+                // TODO : Route to login page
+                console.log("routing")
+              }else{
+                renderData(element);     
+              }
+            });
+          }
+        }
+    };
+    
+    function renderData(products){
+      // TODO: Do something with response 
+    }
+
+    const makePayment = (token,products) => {
+        const body = {
+            token,
+            products
+        }
+        
+        const headers = {
+            "Content-Type": "application/json",
+            "token": Cookies.get('token')
+        }
+    
+        return fetch(process.env.REACT_APP_API_URL+"/buyNow",{
+            method: "POST",
+            headers,
+            body: JSON.stringify(body)
+        }).then(response => {
+            authentication(response)
+        })
+        .catch(error => console.log(error));
+        
+    }
+
     return <div style={{display:'flex',margin:'8% 30px'}}>
         <div style={{display:'flex',flexDirection:'column',marginRight:'10px'}}>
             {
@@ -45,7 +90,12 @@ export default function ProductDetails({match}) {
             <h1>Product Name</h1>
             <h2>Price $2342</h2>
             <div style={{marginTop:'20px'}}>
-                <button style={{marginRight:'30px'}}>Buy Now</button>
+                <StripeCheckout
+                    stripeKey="pk_test_51HgaW6HISAjMedpx6Rx65qvbEpNdhHsyyayo021HcDwMsSHmk9Ei4FnZsEZ1bogeCeG9gPTSdu9FBxgarfA5hlKQ00EJ3URML8"
+                    token={makePayment}
+                    shippingAddress
+                    >
+                </StripeCheckout>
                 <button>Add to Cart</button>
             </div>
         </div>
